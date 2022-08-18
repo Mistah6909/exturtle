5327dc240a445e50b865e9595e4d362977e5aa26
Logan <88503173+Mistah6909@users.noreply.github.com>
2022-08-18 15:10:44 -0500

    square fractal function
    used spare class time to work on a function for a square fractal

--- module.py
+++ module.py
@@ -1,1 +1,25 @@
+import turtle
+class exturtle:
+  def __init__(self,name,size,color):
+    self =  turtle.Turtle()
+    self.size =  turtle.shapesize(size)
+    self.color = turtle.color(color)
+  def move(self,size):
+    turtle.forward(size)
+  def square(self,size):
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+  def sqrFractal(self,dimension,sqr_size):
+    #turtle.degree()
+    for x in range(dimension):
+      turtle.square(sqr_size)
+      turtle.forward()
+      sqr_size -= 10
+
 
